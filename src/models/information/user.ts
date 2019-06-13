3e4d227d244abb871cd320fb31c8023e4ea6d437
nitchakarnpattala <47791591+nitchakarnpattala@users.noreply.github.com>
2019-06-13 14:02:41 +0700

    userCommitLast

--- src/models/information/user.ts
+++ src/models/information/user.ts
@@ -2,11 +2,11 @@ import * as Knex from 'knex';
 
 export class User {
 
-  getUser(db: Knex) {
+  getAllUser(db: Knex) {
     return db('user');
   }
-  
-  postUserWh(db: Knex,username:string) {
+
+  postUser(db: Knex,username:string) {
     return db('user')
     .where("username", username);
   }
--- src/routes/information/user.ts
+++ src/routes/information/user.ts
@@ -14,7 +14,31 @@ const jwt = new Jwt();
 
 const router: Router = Router();
 
-router.get('user-insert', async (req: Request, res: Response) => {
+router.get('user-all', async (req: Request, res: Response) => {
+  let db = req.db;
+  const data = req.body.data;
+  try {
+      const result = await userModel.getAllUser(db);
+      res.send({ ok: true, statusCode: HttpStatus.OK, rows: result });
+  } catch (error) {
+      console.log(error.message);
+      res.send({ ok: false, statusCode: HttpStatus.INTERNAL_SERVER_ERROR, message: error.message });
+  }
+});
+
+  router.post('user', async (req: Request, res: Response) => {
+    let db = req.db;
+    const username = req.body.username;
+    try {
+        const result = await userModel.postUser(db,username);
+        res.send({ ok: true, statusCode: HttpStatus.OK, rows: result });
+    } catch (error) {
+        console.log(error.message);
+        res.send({ ok: false, statusCode: HttpStatus.INTERNAL_SERVER_ERROR, message: error.message });
+    }
+  });
+
+  router.get('user-insert', async (req: Request, res: Response) => {
   let db = req.db;
   const data = req.body.data;
   try {
@@ -24,7 +48,31 @@ router.get('user-insert', async (req: Request, res: Response) => {
       console.log(error.message);
       res.send({ ok: false, statusCode: HttpStatus.INTERNAL_SERVER_ERROR, message: error.message });
   }
+});
 
+  router.get('user-update', async (req: Request, res: Response) => {
+    let db = req.db;
+    const data = req.body.data;
+    const username = req.body.username;
+    try {
+        const result = await userModel.updateUser(db,data,username);
+        res.send({ ok: true, statusCode: HttpStatus.OK, rows: result });
+    } catch (error) {
+        console.log(error.message);
+        res.send({ ok: false, statusCode: HttpStatus.INTERNAL_SERVER_ERROR, message: error.message });
+    }
+});
+
+  router.get('user-delete', async (req: Request, res: Response) => {
+  let db = req.db;
+  const username = req.body.username;
+  try {
+      const result = await userModel.deleteUser(db,username);
+      res.send({ ok: true, statusCode: HttpStatus.OK, rows: result });
+  } catch (error) {
+      console.log(error.message);
+      res.send({ ok: false, statusCode: HttpStatus.INTERNAL_SERVER_ERROR, message: error.message });
+  }
 });
 
 export default router;
